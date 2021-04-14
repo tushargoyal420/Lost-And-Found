0c6483df598cf83d15468ece355303ba2dd4c09e
tusshar77 <61848185+tushargoyal420@users.noreply.github.com>
2021-04-14 17:19:23 +0530

    14-4-2021    --- 5:19

---
+++ LostAndFound/app/src/main/java/com/example/myapplication/t1_login_signup_choice.java
@@ -0,0 +1,46 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class t1_login_signup_choice extends AppCompatActivity {
+
+    Button msignUpButtonSS, mlogInButton2SS ;
+    FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.t1_activity_login_signup_choice);
+
+        mlogInButton2SS = findViewById(R.id.logInButton2SS);
+        msignUpButtonSS = findViewById(R.id.signUpButtonSS);
+
+        fAuth = FirebaseAuth.getInstance();     //for take instance from the our firebase
+
+        if (fAuth.getCurrentUser() != null) {             //if user is already login
+            startActivity(new Intent(getApplicationContext(), t4_WelcomeNote.class));
+            finish();
+        }
+
+        mlogInButton2SS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), t3_LoginActivity.class));
+            }
+        });
+
+        msignUpButtonSS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), t2_SignupActivity.class));
+            }
+        });
+
+    }
